3fe0edf5c1400f30e0ef21cd04aac443bb084c7e
LSYbuilds <builddav2009@gmail.com>
2023-07-25 21:48:30 +0900

    asd

--- src/pages/CommunityWrite.js
+++ src/pages/CommunityWrite.js
@@ -1,29 +1,9 @@
-import React from "react";
-import {CommunityWriteWrapper} from "../css/community-write-style";
+import React from 'react'
 
 const CommunityWrite = () => {
-  return <div>
-    <CommunityWriteWrapper>
-      <div className="top_communityWrite_contents">
-        <div className="communityWrite_contents_inner">
-          <h1 className="top_communityWrite_title">커뮤니티 게시글 작성</h1>
-          <hr className="communityWrite_line"/>
-          <form action="">
-            <select name="" id="" className="communityWrite_board_menu">
-              <option value="">말머리 선택</option>
-              <option value="">말머리</option>
-              <option value="">개머리</option>
-              <option value="">돼지머리</option>
-              <option value="">소머리</option>
-            </select>
-            <button className="communityWrite_board_regi">등록</button>
-            <input type="text" className="communityWrite_board_title" placeholder="제목을 입력해 주세요"/>
-            <textarea name="" id="" cols="30" rows="10" className="communityWrite_board_detail" placeholder="내용을 입력해 주세요"></textarea>
-          </form>
-        </div>
-      </div>
-    </CommunityWriteWrapper>
-  </div>;
-};
+  return (
+    <div>CommunityWrite</div>
+  )
+}
 
-export default CommunityWrite;
+export default CommunityWrite
