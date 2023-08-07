5acf14a508b5cf44544eab51e4f8a80b4dff150b
abismo-github <dev.abismo@gmail.com>
2023-08-07 17:08:18 +0900

    feat: basket 페이지 작업중

--- src/pages/Basket.js
+++ src/pages/Basket.js
@@ -1,47 +1,152 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BasketWrapper } from "../css/basket-style";
+import { getBasketList, getBasketQuantity } from "../api/basketFetch";
+import { useNavigate } from "react-router";
 
 const Basket = () => {
+  const navigate = useNavigate();
+
+  const [basketList, setBasketList] = useState([]);
+  const [count, setCount] = useState(1);
+  const [selectAll, setSelectAll] = useState(false);
+  const [basketQuantity, setBasketQuantity] = useState(null);
+
+  const getBasketData = async () => {
+    try {
+      const data = await getBasketList();
+      const basketWithQuantity = data.map(item => ({ ...item, quantity: 1 }));
+      setBasketList(basketWithQuantity);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // const getQuantityData = async () => {
+  //   try {
+  //     const quantityData = await getBasketQuantity();
+  //     setBasketQuantity(quantityData);
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
+
+  useEffect(() => {
+    getBasketData();
+    // getQuantityData();
+  }, []);
+
+  const onHandleClickPlus = index => {
+    setBasketList(prevBasketList => {
+      const updatedBasketList = [...prevBasketList];
+      updatedBasketList[index] = {
+        ...updatedBasketList[index],
+        quantity: updatedBasketList[index].quantity + 1,
+      };
+      return updatedBasketList;
+    });
+  };
+
+  const onHandleClickMinus = index => {
+    setBasketList(prevBasketList => {
+      const updatedBasketList = [...prevBasketList];
+      if (updatedBasketList[index].quantity > 0) {
+        updatedBasketList[index] = {
+          ...updatedBasketList[index],
+          quantity: updatedBasketList[index].quantity - 1,
+        };
+      }
+      return updatedBasketList;
+    });
+  };
+  const handleSelectAll = () => {
+    setSelectAll(!selectAll);
+    const updatedBasketList = basketList.map(item => ({
+      ...item,
+      selected: !selectAll,
+    }));
+    setBasketList(updatedBasketList);
+  };
+
+  const handleSelectItem = index => {
+    const updatedBasketList = [...basketList];
+    updatedBasketList[index].selected = !updatedBasketList[index].selected;
+    setBasketList(updatedBasketList);
+  };
+
+  const handleRemoveItem = icart => {
+    const updatedBasketList = basketList.filter(item => item.icart !== icart);
+    setBasketList(updatedBasketList);
+  };
+
+  const calculateItemTotal = item => {
+    return item.quantity * item.price;
+  };
+
+  const handleGoToPayment = () => {
+    navigate("/main/payment");
+  };
+
   return (
     <BasketWrapper>
       <div className="basket_inner">
         <h1>장바구니</h1>
         <div className="basket_topline">
           <ul className="basket_topline_list">
-            <li></li>
+            <li>
+              <input
+                type="checkbox"
+                checked={selectAll}
+                onChange={handleSelectAll}
+              />
+            </li>
             <li>사진</li>
             <li>상품명</li>
             <li>수량</li>
             <li>상품금액</li>
           </ul>
         </div>
-        <div className="basket_list">
-          <ul className="basket_goods_list">
-            <li>
-              <div className="basket_choice_box">
-                <input type="checkbox" />
-              </div>
-              <div className="basket_product_img">
-                <img src="#" alt="" />
-              </div>
-              <div>
-                <p>교촌 서울점</p>
-                <p>허니 머시기 후라이드 치킨</p>
-              </div>
-              <div>
-                <button>-</button>
-                <span>1</span>
-                <button>+</button>
-              </div>
-              <div>10000원</div>
-              <div>
-                <button>x</button>
-              </div>
-            </li>
-          </ul>
-        <span className="basket_all_price">100원</span>
-        <button className="basket_box">결제</button>
-        </div>
+        {basketList.map((item, index) => (
+          <li key={index}>
+            <div className="basket_list">
+              <ul className="basket_goods_list">
+                <li>
+                  <div className="basket_choice_box">
+                    <input
+                      type="checkbox"
+                      checked={item.selected}
+                      onChange={() => handleSelectItem(index)}
+                    />
+                  </div>
+                  <div className="basket_product_img">
+                    {/* <img src="#" alt="" />{item.pic} */}
+                    <img src={item.pic} alt="" />
+                  </div>
+                  <div>
+                    {/* <p>{item.name}허니 머시기 후라이드 치킨</p> */}
+                    <p>{item.name}</p>
+                  </div>
+                  <div>
+                    <button onClick={() => onHandleClickMinus(index)}>-</button>
+                    {/* <span>{item.quantity}1</span> */}
+                    <span>{item.quantity}</span>
+                    {/* <span>{count}</span> */}
+                    <button onClick={() => onHandleClickPlus(index)}>+</button>
+                  </div>
+                  {/* <div>{item.price}10000원</div> */}
+                  <div>{calculateItemTotal(item)}</div>
+                  <div>
+                    <button onClick={() => handleRemoveItem(item.icart)}>
+                      x
+                    </button>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </li>
+        ))}
+        <button onClick={handleGoToPayment} className="basket_box">
+          결제
+        </button>
       </div>
     </BasketWrapper>
   );
