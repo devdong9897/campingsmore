f9b822e9d5f0790a223d499e6d8a875c92e1cb7e
devdong <dev.dong9897@gmail.com>
2023-08-08 15:57:53 +0900

    feat: 오더리스트 카테고리 작업

--- src/pages/OrderList.js
+++ src/pages/OrderList.js
@@ -6,18 +6,23 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router";
+import { getOrderDetailPage } from "../api/itemFatch";
 import { OrderListWrapper } from "../css/orderlist-style";
 
 const OrderList = () => {
+  const navigate = useNavigate();
   const [orderlist, setOrderList] = useState([]);
   const [orderListitem, setOrderListItem] = useState([]);
   const [searchText, setSearchText] = useState("");
-  
+  // 현재페이지
+  const [pageNum, setPageNum] = useState(0);
 
   const getOrderListCategory = async () => {
     try {
       const res = await axios.get("/api/item/category");
+      // console.log(res.data);
+
       setOrderList(res.data);
       // 전체 리스트
       getOrderListSearch("");
@@ -38,9 +43,56 @@ const OrderList = () => {
   useEffect(() => {
     getOrderListCategory();
   }, []);
-  
+
   const handleSearch = () => {
     getOrderListSearch(searchText);
+  };
+
+  const handleSort = sortType => {
+    // orderListitem을 정렬하는 로직 추가
+    let sortedItems = [...orderListitem];
+
+    switch (sortType) {
+      case "최신순":
+        sortedItems.sort(
+          (a, b) => new Date(b.createdAt) - new Date(a.createdAt),
+        );
+        break;
+      case "오래된순":
+        sortedItems.sort(
+          (a, b) => new Date(a.createdAt) - new Date(b.createdAt),
+        );
+        break;
+      case "높은가격순":
+        sortedItems.sort((a, b) => b.price - a.price);
+        break;
+      case "낮은가격순":
+        sortedItems.sort((a, b) => a.price - b.price);
+        break;
+      default:
+        break;
+    }
+
+    setOrderListItem(sortedItems);
+  };
+
+  const handleCategoryClick = async categoryId => {
+    console.log("handleCategoryClick : ", categoryId);
+    try {
+      const res = await axios.get(
+        `/api/item/search?cate=${categoryId}&page=1&row=15&sort=0`,
+      );
+      setOrderListItem(res.data.itemList);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const something = iitem => {
+    console.log("실행?");
+    console.log("클릭한 값 인덱스", iitem);
+    getOrderDetailPage(iitem);
+    navigate("main/orderdetail");
   };
 
   return (
@@ -58,14 +110,19 @@ const OrderList = () => {
           </button>
         </div>
         <div className="btn">
-          <button>최신순</button>
-          <button>오래된순</button>
-          <button>높은가격순</button>
-          <button>낮은가격순</button>
+          <button onClick={() => handleSort("최신순")}>최신순</button>
+          <button onClick={() => handleSort("오래된순")}>오래된순</button>
+          <button onClick={() => handleSort("높은가격순")}>높은가격순</button>
+          <button onClick={() => handleSort("낮은가격순")}>낮은가격순</button>
         </div>
         <ul className="order_category">
           {orderlist.map((item, index) => (
-            <li key={index}>{item.name}</li>
+            <li
+              key={index}
+              onClick={() => handleCategoryClick(item.iitemCategory)}
+            >
+              {item.name}
+            </li>
           ))}
         </ul>
         <ul className="order_item_list">
@@ -81,7 +138,7 @@ const OrderList = () => {
                   <p className="item_date">{item.createdAt}</p>
                 </span>
               </div>
-              <div className="content"> 
+              <div className="content">
                 <div className="orderlist_btn">
                   <button className="shopping_basket">
                     장바구니 담기
@@ -90,14 +147,17 @@ const OrderList = () => {
                       className="cart_icon"
                     />
                   </button>
-                  <button className="product_details">
-                    <Link
+                  <button
+                    className="product_details"
+                    onClick={e => something(item.iitem)}
+                  >
+                    <div
                       to={`/main/orderdetail?iitem=${item.iitem}`}
                       className="details_link"
                     >
                       상품상세보기
                       <FontAwesomeIcon icon={faTag} className="tag_icon" />
-                    </Link>
+                    </div>
                   </button>
                 </div>
               </div>
