45b52b9bc20ccb9f9743574f52d6fd38b63bd240
abismo-github <dev.abismo@gmail.com>
2023-08-08 15:52:32 +0900

    feat: communityBulletinBoard 페이지 작업중

---
+++ src/api/communityBulletinBoardFetch.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+const getCommunityBulletinBoarList = async () => {
+    try {
+        const res = await axios.get(`/api/community/boardDetail/1`)
+        const result = res.data;
+        console.log("게시글 상세보기 요청완료")
+        console.log(result)
+        return result
+    }catch(err){
+        console.log(err)
+    }
+    return []
+}
+export {getCommunityBulletinBoarList}
