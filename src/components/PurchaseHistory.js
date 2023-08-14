1c50f222cb46667990236df4219ab4d08d318443
LSYbuilds <builddav2009@gmail.com>
2023-08-14 16:47:15 +0900

    fix: 마이페이지 오류 수정

--- src/components/PurchaseHistory.js
+++ src/components/PurchaseHistory.js
@@ -7,25 +7,27 @@ const PurchaseHistory = ({ purchase }) => {
   return (
     <PurchaseWrapper>
       <h1>구매내역</h1>
-      <ul className="purchase_list">
-        {purchase.map((item, index) => (
-          <li key={index}>
-            {item.itemList.map((subitem, subindex) => (
-              <div className="inner" key={subindex}>
-                <div className="purchase_img">
-                  <img src={subitem.pic}></img>
+      {purchase.length > 0 ? (
+        <ul className="purchase_list">
+          {purchase.map((item, index) => (
+            <li key={index}>
+              {item.itemList.map((subitem, subindex) => (
+                <div className="inner" key={subindex}>
+                  <div className="purchase_img">
+                    <img src={subitem.pic} alt={subitem.name} />
+                  </div>
+                  <div className="purchase_info_box">
+                    <span className="purchase_date">{subitem.paymentDate}</span>
+                    <span className="purchase_info">
+                      <p>{subitem.name}</p>
+                    </span>
+                    <span>{subitem.totalPrice} 원</span>
+                  </div>
                 </div>
-                <div className="purchase_info_box">
-                  <span className="purchase_date">{subitem.paymentDate}</span>
-                  <span className="purchase_info">
-                    <p>{subitem.name}</p>
-                  </span>
-                  <span>{subitem.totalPrice} 원</span>
-                </div>
-              </div>
-            ))}
-          </ul>
-        </>
+              ))}
+            </li>
+          ))}
+        </ul>
       ) : (
         <div className="empty_img_box">
           <img src="../image/cart.png" alt="" />
